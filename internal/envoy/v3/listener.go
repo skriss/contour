e1ae6b0b42cbb2c650141846b79385e1a528f030
Steve Kriss <krisss@vmware.com>
2022-04-21 15:50:08 +0000

    tls inspector and proxy protocol type urls
    Signed-off-by: Steve Kriss <krisss@vmware.com>

--- internal/envoy/v3/listener.go
+++ internal/envoy/v3/listener.go
@@ -29,6 +29,8 @@ import (
 	envoy_config_filter_http_local_ratelimit_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/local_ratelimit/v3"
 	lua "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/lua/v3"
 	envoy_extensions_filters_http_router_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/router/v3"
+	envoy_proxy_protocol_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/listener/proxy_protocol/v3"
+	envoy_tls_inspector_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/listener/tls_inspector/v3"
 	http "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
 	tcp "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/tcp_proxy/v3"
 	envoy_tls_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
@@ -109,6 +111,9 @@ func CodecForVersions(versions ...HTTPVersionType) HTTPVersionType {
 func TLSInspector() *envoy_listener_v3.ListenerFilter {
 	return &envoy_listener_v3.ListenerFilter{
 		Name: wellknown.TlsInspector,
+		ConfigType: &envoy_listener_v3.ListenerFilter_TypedConfig{
+			TypedConfig: protobuf.MustMarshalAny(&envoy_tls_inspector_v3.TlsInspector{}),
+		},
 	}
 }
 
@@ -116,6 +121,9 @@ func TLSInspector() *envoy_listener_v3.ListenerFilter {
 func ProxyProtocol() *envoy_listener_v3.ListenerFilter {
 	return &envoy_listener_v3.ListenerFilter{
 		Name: wellknown.ProxyProtocol,
+		ConfigType: &envoy_listener_v3.ListenerFilter_TypedConfig{
+			TypedConfig: protobuf.MustMarshalAny(&envoy_proxy_protocol_v3.ProxyProtocol{}),
+		},
 	}
 }
 
